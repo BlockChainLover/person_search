4ba422ebce8878ea0ecff6eadd6f2589b21dfed8
Cysu <st.cysu@gmail.com>
2016-11-12 16:53:38 +0800

    Update default pretrain prototxts and script

--- experiments/scripts/pretrain.sh
+++ experiments/scripts/pretrain.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CAFFE=caffe-fast-rcnn
+CAFFE=caffe
 DATASET=psdb
-NET=VGG16
+NET=resnet50
 SNAPSHOTS_DIR=output/${DATASET}_pretrain
 
 LOG="experiments/logs/${DATASET}_pretrain_${NET}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
@@ -11,6 +11,6 @@ cd $(dirname ${BASH_SOURCE[0]})/../../
 
 mkdir -p ${SNAPSHOTS_DIR}
 
-GLOG_logtostderr=1 ${CAFFE}/build/tools/caffe train \
+GLOG_logtostderr=1 mpirun -n 8 ${CAFFE}/build/tools/caffe train \
   -solver models/${DATASET}/${NET}/pretrain_solver.prototxt \
-  -weights data/imagenet_models/${NET}.v2.caffemodel 2>&1 | tee ${LOG}
+  -weights data/imagenet_models/${NET}.caffemodel 2>&1 | tee ${LOG}
